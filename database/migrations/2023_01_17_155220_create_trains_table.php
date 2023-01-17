f9c0639dafa9ad09f773cbc306f9fa1045018f04
Alelisk91 <alessandrofabbretti91@gmail.com>
2023-01-17 17:31:31 +0100

    Aggiunta struttura tabella e esportata in php myAdmin

--- database/migrations/2023_01_17_155220_create_trains_table.php
+++ database/migrations/2023_01_17_155220_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 100)->nullable();
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->tinyInteger('train_code');
+            $table->tinyInteger('carriages_num')->nullable(); 
+            $table->boolean('in_time')->default(1); 
+            $table->boolean('is_cancelled')->default(0);
             $table->timestamps();
         });
     }
