badaa3b44a3150d60ffd6266b1179c3201e57f61
Alelisk91 <alessandrofabbretti91@gmail.com>
2023-01-18 15:46:02 +0100

    Aggiunto seeders con dati fake

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -1,10 +1,9 @@
 <?php
-
 namespace Database\Seeders;
-
+use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Generator as Faker;
 class TrainsTableSeeder extends Seeder
 {
     /**
@@ -12,8 +11,20 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i <= 10; $i++){
+            $new_train = new Train();
+            $new_train->company = $faker->randomElement(['Trenitalia', 'Italo']);
+            $new_train->departure_station = $faker->city();
+            $new_train->arrival_station = $faker->city();
+            $new_train->departure_time = $faker->dateTimeBetween('+0 days', '+1 years');
+            $new_train->arrival_time = $faker->dateTimeBetween($new_train->departure_time, '+1 years');
+            $new_train->train_code = $faker->randomNumber(1, 200);
+            $new_train->carriages_num = $faker->numberBetween(1, 15);
+            $new_train->in_time = $faker->numberBetween(0, 1);
+            $new_train->is_cancelled = $faker->numberBetween(0, 1);
+            $new_train->save();
+        }
     }
 }
